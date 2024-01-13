edd1416034b9c5baebe7ed6c91131099ece70209
Lidia Dias de Souza <99140148+lidia1805@users.noreply.github.com>
2024-01-13 14:36:33 -0300

    Update SignUpScreen.tsx

--- src/screens/SignUpScreen.tsx
+++ src/screens/SignUpScreen.tsx
@@ -7,161 +7,154 @@ import CreateUser from "../use_cases/users/CreateUser";
 const createUser = new CreateUser(new UserService());
 
 export default function SignUpScreen() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
-	const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
+  const navigate = useNavigate();
 
-	const navigate = useNavigate();
+  useEffect(() => {
+    sessionStorage.removeItem('privateKey');
+    sessionStorage.removeItem('hasExecuted');
+  }, []);
 
-	useEffect(() => {
-		sessionStorage.removeItem('privateKey');
-		sessionStorage.removeItem('hasExecuted');
+  async function sendData() {
+    try {
+      const createdUser = await createUser.execute(name, email, password);
 
-	}, []);
+      // Atualizar o estado para exibir a mensagem de sucesso
+      setSuccessMessage("Usuário cadastrado com sucesso!");
 
-	async function SendData() {
-
-        try {
-                const createdUser = await createUser.execute(name, email, password);
-
-                navigate("/login",);
-
-        } catch (error: any) {
-            console.log(error)
-        }
-
+      // Redirecionar para a tela de login após o cadastro
+      setTimeout(() => {
+        navigate("/login");
+      }, 2000); 
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-	return (
-		<div 
-			style={{
-				position: "fixed",
-				top: 0,
-				left: 0,
-				bottom: 0,
-				right: 0,
-				overflow: "auto",
-			}}>
-            <h1 
-				style={{
-					textAlign: "center",
-					marginTop: "50%",
-					font: "icon",
-					fontSize: 35,
-					fontWeight: "bold",
-					color: primary}}>OmegaChat</h1>
-            
-            <div 
-				style={{
-					marginRight: "5%",
-					marginLeft: "5%",
-				}}>
-				<h4 
-					style={{
-					textAlign: "left",
-					marginTop: 0,
-					marginBottom: 0,
-					color: primary}}>Name</h4>
-				<input 
-					style={{
-						width: 352,
-						height: 40,
-						marginTop: 5,
-						marginBottom: 10,
-						backgroundColor: "white",
-						borderWidth: 2,
-						borderRadius: 5,
-						borderColor: primary
-					}}
-					type="text"
-					onChange={e => {
-						setName(e.target.value);
-					}}/>
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        overflow: "auto",
+      }}
+    >
+      <h1
+        style={{
+          textAlign: "center",
+          marginTop: "50%",
+          font: "icon",
+          fontSize: 35,
+          fontWeight: "bold",
+          color: primary,
+        }}
+      >
+        OmegaChat
+      </h1>
 
-			</div>
+      <div style={{ margin: "5%" }}>
+        <h4 style={{ textAlign: "left", marginTop: 0, marginBottom: 0, color: primary }}>
+          Name
+        </h4>
+        <input
+          style={{
+            width: 352,
+            height: 40,
+            marginTop: 5,
+            marginBottom: 10,
+            backgroundColor: "white",
+            borderWidth: 2,
+            borderRadius: 5,
+            borderColor: primary,
+          }}
+          type="text"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+      </div>
 
-			<div 
-				style={{
-					marginRight: "5%",
-					marginLeft: "5%",
-				}}>
-				<h4 
-					style={{
-					textAlign: "left",
-					marginTop: 0,
-					marginBottom: 0,
-					color: primary}}>Email Address</h4>
-				<input 
-					style={{
-						width: 352,
-						height: 40,
-						marginTop: 5,
-						marginBottom: 10,
-						backgroundColor: "white",
-						borderWidth: 2,
-						borderRadius: 5,
-						borderColor: primary
-					}}
-					type="text"
-					onChange={e => {
-						setEmail(e.target.value);
-					}}/>
+      <div style={{ margin: "5%" }}>
+        <h4 style={{ textAlign: "left", marginTop: 0, marginBottom: 0, color: primary }}>
+          Email Address
+        </h4>
+        <input
+          style={{
+            width: 352,
+            height: 40,
+            marginTop: 5,
+            marginBottom: 10,
+            backgroundColor: "white",
+            borderWidth: 2,
+            borderRadius: 5,
+            borderColor: primary,
+          }}
+          type="text"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
 
-			</div>
-			<div 
-				style={{
-					marginRight: "5%",
-					marginLeft: "5%",
-				}}>
-				<h4 
-					style={{
-					textAlign: "left",
-					marginTop: 0,
-					marginBottom: 0,
-					color: primary}}>Password</h4>
-				<input 
-					style={{
-						width: 352,
-						height: 40,
-						marginTop: 5,
-						backgroundColor: "white",
-						borderWidth: 2,
-						borderRadius: 5,
-						borderColor: primary
-					}}
-					type="text"
-					onChange={e => {
-						setPassword(e.target.value);
-					}}/>
+      <div style={{ margin: "5%" }}>
+        <h4 style={{ textAlign: "left", marginTop: 0, marginBottom: 0, color: primary }}>
+          Password
+        </h4>
+        <input
+          style={{
+            width: 352,
+            height: 40,
+            marginTop: 5,
+            backgroundColor: "white",
+            borderWidth: 2,
+            borderRadius: 5,
+            borderColor: primary,
+          }}
+          type="text"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
 
-			</div>
-			<button
-				style={{
-					marginRight: "5%",
-					marginLeft: "6%",
-					marginTop: "30%",
-					height: 50,
-					width: 352,
-					backgroundColor: primary,
-					color: "white",
-					borderRadius: 5,
-					fontSize: 15
+      <button
+        style={{
+          marginRight: "5%",
+          marginLeft: "6%",
+          marginTop: "5%",
+          height: 50,
+          width: 352,
+          backgroundColor: primary,
+          color: "white",
+          borderRadius: 5,
+          fontSize: 15,
+        }}
+        onClick={sendData}
+      >
+        Sign Up
+      </button>
 
-				}}
-				onClick={SendData}>Sign Up</button>
+      {successMessage && (
+        <p style={{ color: "green", marginTop: "1%", textAlign: "center" }}>
+          {successMessage}
+        </p>
+      )}
 
-            <div style={{ marginTop: "2%", textAlign: "center" }}>
-					<p
-					style={{ color: primary, textDecoration: "underline", cursor: "pointer" }}
-					onClick={() => navigate("/login")}
-					>
-					Ir para tela de login
-					</p>
-				</div>
-                
-
-		</div>
-			
-	)
+      <div style={{ marginTop: "2%", textAlign: "center" }}>
+        <p
+          style={{ color: primary, textDecoration: "underline", cursor: "pointer" }}
+          onClick={() => navigate("/login")}
+        >
+          Ir para tela de login
+        </p>
+      </div>
+    </div>
+  );
 }
